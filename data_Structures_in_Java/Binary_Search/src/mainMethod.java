832bc4c7ab06ac2e6ad629829b6f707cd922ae4a
Victor Aladele <valad@gatech.edu>
2019-02-09 13:23:32 -0500

    added

--- data_Structures_in_Java/Binary_Search/src/mainMethod.java
+++
@@ -1,7 +0,0 @@
-public class mainMethod {
-    public static void main(String[] args){
-        int[] array = {1, 2, 3, 4, 5, 8, 10, 18, 20};
-        //Binary_search.recursion(array, 10);
-        Binary_search.iteration(array, 40);
-    }
-}
--- data_Structures_in_Java/linkedLists/src/Node.java
+++
@@ -1,6 +0,0 @@
-public class Node {
-
-    int data;
-    Node next;
-
-}
