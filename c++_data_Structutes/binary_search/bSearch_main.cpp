c4a44c6cb3627c94e1a9c581a9494424c6c303ce
Victor Aladele <valad@gatech.edu>
2019-03-01 14:15:55 -0500

    Binary search done

--- c++_data_Structutes/binary_search/bSearch_main.cpp
+++ c++_data_Structutes/binary_search/bSearch_main.cpp
@@ -4,6 +4,6 @@ int main(){
     int a[] = {1, 2, 3, 4, 8, 9, 12};
     //std::cout << sizeof(a) << std::endl;
     b_search <int> obj(a, 7);
-    obj.search();
+    obj.search(0, a);
     return 0;
 }
--- c++_data_Structutes/binary_search/b_search.cpp
+++ c++_data_Structutes/binary_search/b_search.cpp
@@ -2,12 +2,14 @@
 template <class T> 
 class b_search{
     int size;
+    bool found;
+    int mid, left, right;
     T* array;
-    T leftArray[], rightArray[];
+    //T leftArray[], rightArray[];
 public:
     b_search(T arrayIn[], int sizeIn);
     ~b_search();
-    bool search();
+    int search(T number, T arr[]);
     
     
 };
@@ -17,6 +19,8 @@ b_search<T>::b_search(T arrayIn[], int sizeIn){
     std::cout << "Constructor initiated\n";
     array = arrayIn;
     size = sizeIn;
+    left = 0;
+    right = size;
 }
 
 template <typename T> 
@@ -24,9 +28,78 @@ b_search<T>::~b_search(){
     std::cout << "Destructor called, program ended\n";
 }
 
-template <typename T> 
-bool b_search<T>::search(){
-    std::cout << "hey \n";
-}
 
+// template <typename T> 
+// int b_search<T>::search(T number, T arr[]){
+
+//     if(left < right){
+//         mid = left + (right - left)/2;
+//         std::cout << left << " " << right << " " << mid << "\n";
+//         if(array[mid] > number){
+//             T leftArray[mid+1];
+//             right = mid;
+//             for(int i = left; i < mid + 1; i++){
+//                 leftArray[i] = array[i];
+//             }
+//             search(number, leftArray);
+//         }
+
+//         else if(array[mid] < number){
+//             T rightArray[right + 1];
+//             left = mid + 1;
+//             for(int i = mid + 1; i < right + 1; i++){
+//                 rightArray[i] = array[i];
+//             }
+//             search(number, rightArray);
+//         }
+
+//         else{
+//             found = true;
+//             std::cout << "The number can be found in the list\n";
+//         }
+//     }
+    
+//     else{
+//         found = false;
+//         std::cout << "The number is not in the list\n";
+//     }
+// }
+
+
+template <typename T> 
+int b_search<T>::search(T number, T arr[]){
+
+    if(left < right){
+        mid = left + (right - left)/2;
+        std::cout << left << " " << right << " " << mid << "\n";
+        if(array[mid] > number){
+            T leftArray[mid];
+            right = mid;
+            for(int i = left; i < mid; i++){
+                leftArray[i] = array[i];
+            }
+            search(number, leftArray);
+        }
+
+        else if(array[mid] < number){
+            T rightArray[right];
+            left = mid + 1;
+            for(int i = mid; i < right; i++){
+                rightArray[i] = array[i];
+            }
+            search(number, rightArray);
+        }
+
+        else{
+            found = true;
+            std::cout << "The number can be found at position: " << mid + 1 << "\n";
+        }
+    }
+    
+    else{
+        found = false;
+        std::cout << "The number is not in the list\n";
+    }
+}
+//RRTs, A*
 
